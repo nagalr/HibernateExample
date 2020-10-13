441238c78c1a81b8d29ab31380a972d1bef6e70a
nagalr <nagalr@gmail.com>
2020-10-13 17:49:09 +0300

    finalize before closing the sample

--- src/main/java/com/example/HibernateTest.java
+++ src/main/java/com/example/HibernateTest.java
@@ -16,17 +16,20 @@ public class HibernateTest {
         Department department = new Department("java");
         session.save(department);
 
-        session.save(new Employee("Jakab Gipsz",department));
-        session.save(new Employee("Captain Nemo",department));
+        session.save(new Employee("Jakab Gipsz", department));
+        session.save(new Employee("Captain Nemo", department));
 
         session.getTransaction().commit();
 
         Query q = session.createQuery("From Employee ");
 
         List<Employee> resultList = q.list();
-        System.out.println("\nnum of employees: " + resultList.size() +"\n");
+        System.out.println("\nnum of employees: " + resultList.size() + "\n");
         for (Employee next : resultList) {
             System.out.println("next employee: " + next);
         }
+
+        session.close();
+
     }
 }
